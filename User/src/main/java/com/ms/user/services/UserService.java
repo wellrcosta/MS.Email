284ba93f4424b05fb6909750324b0d3ea286dfe9
Wellington Reis <wellington.costa.97@gmail.com>
2023-10-17 18:33:55 -0300

    Add rabbitMQ Producer on User

--- User/src/main/java/com/ms/user/services/UserService.java
+++ User/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.IUserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,13 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     private final IUserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(IUserRepository userRepository) {
+    public UserService(IUserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel saveUser(UserModel userModel) {
-        return userRepository.save(userModel);
+        var user = userRepository.save(userModel);
+        userProducer.publishMessageEmail(user);
+        return user;
     }
 }
